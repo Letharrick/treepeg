c2f5a18dd94eab56f56bce1ff752368775cebf55
Justin <justin.choi.developer@gmail.com>
2019-12-07 16:30:11 -0600

    dec/07(sat): comment out unneccessary codes.

--- controller.js
+++ controller.js
@@ -6,21 +6,27 @@ let sampleObj = {
     tree_id: '111111'
 }
 
+// demo of how to call getDataset()
 async function onClickHandler() {
-    let listNode = document.createElement("li");
-    let textNode = document.createTextNode("Added List Elements...");
-    listNode.appendChild(textNode);
-    document.getElementById("list").appendChild(listNode);
+    // let listNode = document.createElement("li");
+    // let textNode = document.createTextNode("Added List Elements...");
+    // listNode.appendChild(textNode);
+    // document.getElementById("list").appendChild(listNode);
 
-    let dataset = await fetchAPI(base, sampleObj).catch((error) => error)
-    console.log(dataset);
+    let dataset = await getDataset() // need await!
+    console.log(dataset)
 }
 
 
 // ref: Assign Fetch Response To Variable
 //      https://www.reddit.com/r/learnjavascript/comments/9zo92w/assign_fetch_response_to_variable/
-// async function getDataset() {
-//     let dataset = await fetchAPI(base, sampleObj).catch((error) => error)
-//     // let dataset = await fetch.fetchAPI(base, sampleObj).catch((error) => error) //[Node.js Version]
-//     console.log(document.getElementById("list"));
-// }
+async function getDataset() {
+    
+    // let dataset = await fetch.fetchAPI(base, sampleObj).catch((error) => error) //[Node.js Version]
+    let dataset = await fetchAPI(base, sampleObj).catch((error) => error)
+
+    // filter response dataset (not required)
+    
+    // return resulting data
+    return dataset
+}
